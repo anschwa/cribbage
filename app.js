bebfa2d61e6cbae7428957ad3ef98edbfdb5a01f
Adam Schwartz <adam@anschwa.com>
2023-06-19 23:42:45 -0400

    2023-06-19

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// app.js
+
 const g = (selector) => {
   const el = document.getElementById(selector);
   return el;
@@ -9,14 +11,31 @@ const toggleDisplayNone = (selector) => {
 };
 
 const toggleGameBoardAndMenu = () => {
-  toggleDisplayNone("game-menu");
-  toggleDisplayNone("game-board");
+  toggleDisplayNone("gameMenu");
+  toggleDisplayNone("gameBoard");
+  toggleDisplayNone("newGameButton");
 };
 
-const handlePlayButton = () => {
+const handlePlayButton = (event) => {
+  event.preventDefault();
+
+  const { numPlayers, maxScore } = getGameMenuFormData();
+
   toggleGameBoardAndMenu();
 };
 
 const handleNewGameButton = () => {
+  if (!window.confirm("Are you sure you want to start a new game?")) {
+    return;
+  }
+
   toggleGameBoardAndMenu();
+};
+
+const getGameMenuFormData = () => {
+  const formData = new FormData(g("gameMenu"));
+  const numPlayers = Number(formData.get("numPlayers"));
+  const maxScore = Number(formData.get("maxScore"));
+
+  return { numPlayers, maxScore };
 };
